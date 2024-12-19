b3c05f350ddc40af9b09fcf44c6016afee37f194
Anderson Lemos Camelo <andersonlemos83@gmail.com>
2024-12-19 09:16:53 -0300

    Commit inicial.

---
+++ core/src/main/java/br/com/alc/ecommerce/checkout/core/EcommerceCheckoutCoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.alc.ecommerce.checkout.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceCheckoutCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceCheckoutCoreApplication.class, args);
+    }
+
+}
---
+++ infrastructure/src/main/java/br/com/alc/ecommerce/checkout/infrastructure/EcommerceCheckoutInfrastructureApplication.java
@@ -0,0 +1,13 @@
+package br.com.alc.ecommerce.checkout.infrastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceCheckoutInfrastructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceCheckoutInfrastructureApplication.class, args);
+    }
+
+}
