beda7b8cca14ca30e3f9b3d7c8f5c5345f858733
Anderson Lemos Camelo <andersonlemos83@gmail.com>
2025-01-22 15:42:56 -0300

    Ajusta complilação do maven mult module.

--- core/src/main/java/br/com/alc/ecommerce/checkout/core/EcommerceCheckoutCoreApplication.java
+++ core/src/main/java/br/com/alc/ecommerce/checkout/core/EcommerceCheckoutCoreApplication.java
@@ -1,9 +1,7 @@
 package br.com.alc.ecommerce.checkout.core;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"br.com.alc.ecommerce.checkout.core"})
 public class EcommerceCheckoutCoreApplication {
 
     public static void main(String[] args) {
