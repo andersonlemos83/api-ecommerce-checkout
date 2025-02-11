6b8780a2a6d3c091fb8378bcf848b47c5653d70a
Anderson Lemos Camelo <andersonlemos83@gmail.com>
2025-02-10 21:09:30 -0300

    Ajustantando débitos do sonar.

--- alternative-infrastructure/src/test/java/br/com/alc/ecommerce/checkout/infrastructure/helper/testcontainers/impl/ContainerManagerKafka.java
+++ alternative-infrastructure/src/test/java/br/com/alc/ecommerce/checkout/infrastructure/helper/testcontainers/impl/ContainerManagerKafka.java
@@ -5,6 +5,7 @@ import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.KafkaContainer;
 
 import static java.util.Arrays.asList;
+import static org.testcontainers.utility.DockerImageName.parse;
 
 @Log4j2
 public class ContainerManagerKafka extends AbstractContainerManager {
@@ -13,7 +14,7 @@ public class ContainerManagerKafka extends AbstractContainerManager {
 
     @Override
     protected GenericContainer createContainer() {
-        KafkaContainer container = new KafkaContainer("5.4.3");
+        KafkaContainer container = new KafkaContainer(parse("confluentinc/cp-kafka:5.4.3"));
         container.setPortBindings(asList(SERVICE_PORT + ":" + SERVICE_PORT));
         container.withExposedPorts(SERVICE_PORT);
         return container;
